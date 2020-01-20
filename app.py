32698499441b8e1bc59f275f6625c11042b312fb
teguhatma <teguhatmayudha@gmail.com>
2020-01-20 23:51:57 +0700

    1.5.4 Pendifinisian Model

--- app.py
+++ app.py
@@ -12,6 +12,25 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 moment = Moment(app)
 
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id  = db.Column(db.Integer, primary_key=True)
+    name    = db.Column(db.String(64), unique=True)
+
+    def __repr__(self):
+        return '<Role {}>'.format(self.name)
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id  = db.Column(db.Integer, primary_key=True)
+    username    = db.Column(db.String(64), unique=True, index=True)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+
 @app.route('/', methods=['GET','POST'])
 def index():
     form = User()
