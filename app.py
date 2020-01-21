807477d587cf0ad4cbaaba3c60b2311c887d5158
teguhatma <teguhatmayudha@gmail.com>
2020-01-21 20:00:08 +0700

    1.5.5 Relasi

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ class Role(db.Model):
 
     id  = db.Column(db.Integer, primary_key=True)
     name    = db.Column(db.String(64), unique=True)
+    users   = db.relationship('User', backref='role')
 
     def __repr__(self):
         return '<Role {}>'.format(self.name)
@@ -26,6 +27,7 @@ class User(db.Model):
 
     id  = db.Column(db.Integer, primary_key=True)
     username    = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
